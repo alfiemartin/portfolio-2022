3fc0d531f86ccd8a46e7e24dfb3c9a10effa26ef
Alfie Martin <alfie.martin@remarkable.net>
2022-03-17 16:17:45 +0000

    fix build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import PageTemplate from "../components/Templates";
+import PageTemplate from "../components/Templates/index";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
