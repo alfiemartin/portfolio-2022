b9780eb8a00581d347f555d2e4c61071822bcb89
Alfie Martin <alfie.martin@remarkable.net>
2022-03-25 13:15:16 +0000

    show game animations

--- components/Introduction/index.tsx
+++ components/Introduction/index.tsx
@@ -1,11 +1,20 @@
 import alfieImage from "../../public/alfie-full.jpg";
 import Modal from "react-modal";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { swiperButtonClasses } from "../SlidesNav";
 
 export const Introduction = () => {
   const [showGame, setShowGame] = useState(false);
+  const [revealGame, setRevealGame] = useState(false);
+
+  const handleModalClose = () => {
+    setRevealGame(false);
+
+    setTimeout(() => {
+      setShowGame(false);
+    }, 1000)
+  }
 
   return (
     <div className="h-100-accom flex justify-center items-center">
@@ -29,12 +38,14 @@ export const Introduction = () => {
           </h5>
         </div>
       </div>
-      <Modal isOpen={showGame} className={"bg-red-200 bottom-0 absolute w-screen h-100-accom z-20"} overlayClassName="z-20">
-        <div className="w-full h-100-accom absolute bottom-0 flex justify-between items-end p-12 pointer-events-none">
-          <button className={`${swiperButtonClasses} pointer-events-auto`} onClick={() => setShowGame(false)} >Quit Game</button>
-          <button className={`${swiperButtonClasses} pointer-events-auto`}>About This Projects</button>
+      <Modal isOpen={showGame} onAfterOpen={() => setRevealGame(true)} className={"bottom-0 absolute w-screen h-100-accom z-20"} overlayClassName="z-20">
+        <div className={`w-full h-full transition-opacity duration-1000 ${revealGame ? 'opacity-100' : 'opacity-0'}`}>
+          <div className="w-full h-100-accom absolute bottom-0 flex justify-between items-end p-12 pointer-events-none">
+            <button className={`${swiperButtonClasses} pointer-events-auto`} onClick={handleModalClose} >Quit Game</button>
+            <button className={`${swiperButtonClasses} pointer-events-auto`}>About This Projects</button>
+          </div>
+          <iframe height={'100%'} width={'100%'} src='https://tapflash-77edb.web.app/' />
         </div>
-        <iframe height={'100%'} width={'100%'} src='https://tapflash-77edb.web.app/' />
       </Modal>
     </div>
   );
