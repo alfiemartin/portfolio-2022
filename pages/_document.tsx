ea72eb5c183424b572b7953114dd7744358dae29
Alfie Martin <alfie.martin@remarkable.net>
2022-03-25 13:53:54 +0000

    lighthouse stuff

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Document = () => {
   return (
-    <Html>
+    <Html lang='en-GB' >
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
