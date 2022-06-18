d2106eaabc0fdeadedcf025409e3f2ca238eb582
alfiemartin <alfie.martin@remarkable.net>
2022-06-18 18:06:38 +0100

    removed game

--- components/Introduction/index.tsx
+++ components/Introduction/index.tsx
@@ -1,30 +1,7 @@
 import alfieImage from "../../public/alfie-full.jpg";
 import Image from "next/image";
-import { useState } from "react";
-import { useGlobalContext } from "../../pages/_app";
-import { GameModal } from "../GameModal";
 
 export const Introduction = () => {
-  const [showGame, setShowGame] = useState(false);
-  const [revealGame, setRevealGame] = useState(false);
-  const {setPageTitle} = useGlobalContext();
-
-  const handleModalClose = () => {
-    if(setPageTitle) setPageTitle('Welcome');
-    
-    setRevealGame(false);
-
-    setTimeout(() => {
-      setShowGame(false);
-    }, 700)
-  }
-
-  const handleAfterModalOpen = () => {
-    if(setPageTitle) setPageTitle('My First Game');
-
-    setRevealGame(true);
-  }
-
   return (
     <div className="h-100-accom flex justify-center items-center">
       <div className="flex gap-4">
@@ -41,13 +18,8 @@ export const Introduction = () => {
           <h1>Alfie Martin</h1>
           <h3 className="mb-10">Front-end Developer</h3>
           <h5 className="">Check out what i&apos;ve been doing ➡️</h5>
-          <h5 className="mt-auto">
-            .... Or play a quick{' '} 
-            <button className="underline cursor-pointer hover:text-blue-800 dark:hover:text-blue-500" onClick={() => setShowGame(true)}>game?</button>
-          </h5>
         </div>
       </div>
-      <GameModal handleAfterModalOpen={handleAfterModalOpen} handleModalClose={handleModalClose} revealGame={revealGame} showGame={showGame} />
     </div>
   );
 };
