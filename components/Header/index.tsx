95d407913f49f0280d87ad6f2e4164af5bf417df
alfiemartin <alfie.martin@remarkable.net>
2022-06-18 18:47:32 +0100

    fixing the darkmode toggle

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,38 +1,14 @@
 import Image from "next/image";
-import { useContext, useEffect, useState } from "react";
 import ReactSwitch from "react-switch";
 import { useHeadroom } from "../../hooks/useHeadroom";
 import siteLogo from "../../public/error.ico";
 import moonImage from "../../public/moon.png";
 import sunImage from "../../public/sun.png";
-import Cookies from "js-cookie";
 import { useGlobalContext } from "../../pages/_app";
 
 export const Header = () => {
   const scrollingDown = useHeadroom();
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const globalState = useGlobalContext();
-  const [pageTitle, setPageTitle] = useState(globalState.pageTitle);
-
-  useEffect(() => {
-    setIsDarkMode(Cookies.get("dark-mode") === "true" ? true : false);
-  }, []);
-
-  useEffect(() => {
-    if (isDarkMode) {
-      document.documentElement.classList.add("dark");
-
-      const longExpiryDate = new Date(new Date().getTime() * 2);
-      Cookies.set("dark-mode", "true", { expires: longExpiryDate });
-    } else {
-      document.documentElement.classList.remove("dark");
-      Cookies.remove("dark-mode");
-    }
-  }, [isDarkMode]);
-
-  useEffect(() => {
-    setPageTitle(globalState.pageTitle);
-  }, [globalState.pageTitle]);
+  const {isDark, pageTitle, setIsDark} = useGlobalContext();
 
   return (
     <header className='w-full dark:text-white'>
@@ -52,7 +28,11 @@ export const Header = () => {
         <div className='flex justify-end flex-1 gap-3'>
           <ul className='flex gap-3'>
           </ul>
-          <ReactSwitch onChange={() => setIsDarkMode(prev => !prev)} checked={isDarkMode} checkedHandleIcon={Checked} uncheckedHandleIcon={Unchecked} checkedIcon={<></>} uncheckedIcon={<></>} />
+          {
+            typeof isDark !== undefined && (
+              <ReactSwitch onChange={() => setIsDark(prev => !prev)} checked={isDark} checkedHandleIcon={Checked} uncheckedHandleIcon={Unchecked} checkedIcon={<></>} uncheckedIcon={<></>} />
+            )
+          }
         </div>
       </nav>
     </header>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,41 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import PageTemplate from "../components/Foundations";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Cookies from "js-cookie";
 
 type GlobalContext = {
-  pageTitle?: string;
-  setPageTitle?: React.Dispatch<React.SetStateAction<string>>;
+  pageTitle: string;
+  setPageTitle: React.Dispatch<React.SetStateAction<string>>;
+  isDark: boolean;
+  setIsDark: React.Dispatch<React.SetStateAction<boolean>>;
 };
-const GlobalContext = React.createContext<GlobalContext>({});
-export const useGlobalContext = () => useContext(GlobalContext);
+const GlobalContext = React.createContext<GlobalContext | null>(null);
+export const useGlobalContext = () => useContext(GlobalContext) as GlobalContext;
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [pageTitle, setPageTitle] = useState("Welcome");
-  const [isDark, setIsDark] = useState(Cookies.get("dark-mode") === "true" ? true : false);
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    setIsDark(Cookies.get("dark-mode") === "true");
+    console.log("setIsDark");
+  }, [])
+
+  useEffect(() => {
+    if (isDark) {
+      document.documentElement.classList.add("dark");
+
+      const longExpiryDate = new Date(new Date().getTime() * 2);
+      Cookies.set("dark-mode", "true", { expires: longExpiryDate });
+    } else {
+      document.documentElement.classList.remove("dark");
+      Cookies.remove("dark-mode");
+    }
+  }, [isDark]);
 
   return (
-    <GlobalContext.Provider value={{ pageTitle, setPageTitle }}>
+    <GlobalContext.Provider value={{ pageTitle, setPageTitle, isDark, setIsDark }}>
       <PageTemplate>
         <Component {...pageProps} />
       </PageTemplate>
