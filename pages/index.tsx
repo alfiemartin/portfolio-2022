241d12aa8e076d320ee1333de444dd6eb56f4f0c
Alfie <alfie.martin@hotmail.co.uk>
2022-06-18 23:52:49 +0100

    updateed packages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import * as THREE from "three";
 import React, { MouseEvent, useEffect, useRef, useState } from "react";
 import type { NextPage } from "next";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -8,6 +9,7 @@ import { Projects } from "../components/Projects";
 import { WorkExperience } from "../components/WorkExperience";
 import { SlidesButtonNav, SlidesNav } from "../components/SlidesNav";
 import "swiper/css";
+import { Canvas, useFrame } from "@react-three/fiber";
 
 const Home: NextPage = () => {
   const globalState = useGlobalContext();
@@ -62,7 +64,7 @@ const Home: NextPage = () => {
   useEffect(() => {
     setTimeout(() => {
       setShowNav(false);
-    }, 5000);
+    }, 3000);
   }, []);
 
   return (
@@ -94,8 +96,38 @@ const Home: NextPage = () => {
         getPageTitle={getPageTitle}
         swiper={swiper}
       />
+      <div className='absolute w-screen h-screen inset-0'>
+        {typeof window !== "undefined" && (
+          <Canvas>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <Box position={[-1.2, 0, 0]} />
+            <Box position={[1.2, 0, 0]} />
+          </Canvas>
+        )}
+      </div>
     </>
   );
 };
+
+function Box(props: JSX.IntrinsicElements["mesh"]) {
+  const ref = useRef<THREE.Mesh>(null!);
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+  useFrame((state, delta) => (ref.current.rotation.x += 0.01));
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
 
 export default Home;
