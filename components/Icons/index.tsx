78d649794f25b3dcf6c5d3dcbd142b464910d9aa
Alfie Martin <alfie.martin@experian.com>
2023-05-18 21:12:56 +0100

    icons

---
+++ components/Icons/index.tsx
@@ -0,0 +1,105 @@
+import { motion, useAnimate } from "framer-motion";
+import { useEffect, useState } from "react";
+import {
+  SiReact,
+  SiTypescript,
+  SiMongodb,
+  SiExpress,
+  SiGraphql,
+  SiApollographql,
+  SiNodedotjs,
+} from "react-icons/si";
+import { getTheme } from "../../utils";
+
+interface IconsProps {
+  size?: number;
+}
+
+export const IReact = ({ size = 50 }: IconsProps) => {
+  return (
+    <motion.div whileHover={{ rotateZ: 360 }} transition={{ type: "spring" }}>
+      <SiReact size={size} />
+    </motion.div>
+  );
+};
+
+export const IMongoDb = ({
+  size,
+  defaultColour,
+}: IconsProps & { defaultColour: string }) => (
+  <motion.div
+    initial={{ "--colour": defaultColour } as any}
+    whileHover={
+      {
+        rotateZ: 30,
+        "--colour": getTheme()?.colors.green["700"],
+      } as any
+    }
+    transition={{ type: "spring", bounce: 0.6 }}
+    key={defaultColour}
+  >
+    <SiMongodb style={{ fill: "var(--colour)" }} key={"3"} size={size} />
+  </motion.div>
+);
+
+export const IGraphQL = ({ size }: IconsProps) => (
+  <motion.div whileHover={{ rotateZ: 360 }} transition={{ type: "spring" }}>
+    <SiGraphql size={size} />
+  </motion.div>
+);
+
+export const IApollo = ({ size }: IconsProps) => (
+  <motion.div whileHover={{ rotateZ: -360 }} transition={{ type: "spring" }}>
+    <SiApollographql size={size} />
+  </motion.div>
+);
+
+export const IExpress = ({ size }: IconsProps) => (
+  <motion.div
+    whileHover={{ scale: 1.2 }}
+    transition={{ type: "spring", bounce: 0.6 }}
+  >
+    <SiExpress size={size} />
+  </motion.div>
+);
+
+export const INodeJS = ({ size }: IconsProps) => (
+  <motion.div whileHover={{ rotateZ: 360 }} transition={{ type: "spring" }}>
+    <SiNodedotjs size={size} />
+  </motion.div>
+);
+
+export const ITypescript = ({ size = 50 }: IconsProps) => {
+  const [animate, setAnimate] = useState<"enter" | "leave" | null>(null);
+  const [scope, animateFn] = useAnimate();
+
+  const duration_ms = 200;
+  const duration_s = duration_ms / 1000;
+
+  useEffect(() => {
+    if (animate) {
+      const enterAnimation = [0, 10, -10, 0];
+
+      animateFn(
+        "*",
+        {
+          x:
+            animate === "enter"
+              ? enterAnimation
+              : enterAnimation.map((x) => (x *= -1)),
+        },
+        { duration: duration_s, ease: [0, 0, 0, 0] }
+      );
+    }
+  }, [animate]);
+
+  return (
+    <motion.div
+      onMouseEnter={() => setAnimate("enter")}
+      onMouseLeave={() => setAnimate("leave")}
+      ref={scope}
+    >
+      <SiTypescript size={size} />
+    </motion.div>
+  );
+};
--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -1,61 +1,17 @@
 import { SlideTemplate } from "../Foundations";
 import { ProjectCard } from "../ProjectCard/ProjectCard";
-import {
-  SiTypescript,
-  SiMongodb,
-  SiExpress,
-  SiReact,
-  SiGraphql,
-  SiApollographql,
-  SiNodedotjs,
-} from "react-icons/si";
-import { motion, useAnimate } from "framer-motion";
-import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import { getTheme } from "../../utils";
 import { useGlobalContext } from "../../context/GlobalContext";
-
-const IReact = ({ size = 50 }: { size?: number }) => {
-  return (
-    <motion.div whileHover={{ rotateZ: 360 }} transition={{ type: "spring" }}>
-      <SiReact size={size} />
-    </motion.div>
-  );
-};
-
-const ITypescript = ({ size = 50 }: { size?: number }) => {
-  const [animate, setAnimate] = useState<"enter" | "leave" | null>(null);
-  const [scope, animateFn] = useAnimate();
-
-  const duration_ms = 200;
-  const duration_s = duration_ms / 1000;
-
-  useEffect(() => {
-    if (animate) {
-      const enterAnimation = [0, 10, -10, 0];
-
-      animateFn(
-        "*",
-        {
-          x:
-            animate === "enter"
-              ? enterAnimation
-              : enterAnimation.map((x) => (x *= -1)),
-        },
-        { duration: duration_s, ease: [0, 0, 0, 0] }
-      );
-    }
-  }, [animate]);
-
-  return (
-    <motion.div
-      onMouseEnter={() => setAnimate("enter")}
-      onMouseLeave={() => setAnimate("leave")}
-      ref={scope}
-    >
-      <SiTypescript size={size} />
-    </motion.div>
-  );
-};
+import {
+  IApollo,
+  IExpress,
+  IMongoDb,
+  INodeJS,
+  IReact,
+  ITypescript,
+} from "../Icons";
+import { IGraphQL } from "../Icons/index";
 
 export const Projects = () => {
   const { isDark } = useGlobalContext();
@@ -75,51 +31,11 @@ export const Projects = () => {
           icons={[
             <IReact key={"1"} size={50} />,
             <ITypescript key={"2"} size={50} />,
-            <motion.div
-              initial={{ "--colour": defaultColour } as any}
-              whileHover={
-                {
-                  rotateZ: 30,
-                  "--colour": getTheme()?.colors.green["700"],
-                } as any
-              }
-              transition={{ type: "spring", bounce: 0.6 }}
-              key={isDark ? "3Dark" : "3Light"}
-            >
-              <SiMongodb
-                style={{ fill: "var(--colour)" }}
-                key={"3"}
-                size={50}
-              />
-            </motion.div>,
-            <motion.div
-              whileHover={{ scale: 1.2 }}
-              transition={{ type: "spring", bounce: 0.6 }}
-              key={"4"}
-            >
-              <SiExpress key={"5"} size={50} />
-            </motion.div>,
-            <motion.div
-              whileHover={{ rotateZ: 360 }}
-              transition={{ type: "spring" }}
-              key={"6"}
-            >
-              <SiGraphql size={50} />
-            </motion.div>,
-            <motion.div
-              whileHover={{ rotateZ: 360 }}
-              transition={{ type: "spring" }}
-              key="7"
-            >
-              <SiApollographql key={"7"} size={50} />
-            </motion.div>,
-            <motion.div
-              whileHover={{ rotateZ: 360 }}
-              transition={{ type: "spring" }}
-              key={"8"}
-            >
-              <SiNodedotjs key={"8"} size={50} />
-            </motion.div>,
+            <IGraphQL key={"3"} size={50} />,
+            <IMongoDb key={"4"} defaultColour={defaultColour} size={50} />,
+            <IApollo key={"5"} size={50} />,
+            <IExpress key={"6"} size={50} />,
+            <INodeJS key={"7"} size={50} />,
           ]}
         />
         <ProjectCard
@@ -128,9 +44,7 @@ export const Projects = () => {
             social media campain to promote a mental health app."
           content={<div></div>}
         />
-        <div className="mt-8">
-          <h3>&apos;Older Projects&apos;</h3>
-        </div>
+        <h3 className="mt-8">&apos;Older Projects&apos;</h3>
         <ProjectCard
           title="Previous Portfolios"
           description="At the height on the COVID lockdown, me and my brother lauched a
